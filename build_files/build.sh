2c70e773ce38bd22f5bbe0c19e7c8fc7a47d268e
Thomas Blarre <thomas@blarre.net>
2026-09-22 17:53:26 +0200

    Install netbird - try #1

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,6 +17,20 @@ dnf5 install -y tmux htop cockpit cockpit-podman cockpit-ostree
 # Install dependencies for argon one - maybe python-gpiozero is needed
 dnf5 install -y python-pigpio python3-rpi-gpio2 i2c-tools python3-i2c-tools smartmontools
 
+# Install netbird
+# Add netbird repo
+cat <<-EOF | tee /etc/yum.repos.d/netbird.repo
+[NetBird]
+name=NetBird
+baseurl=https://pkgs.netbird.io/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+repo_gpgcheck=1
+EOF
+# Install the package
+dnf5 install -y netbird
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
