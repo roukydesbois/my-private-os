5e9f7ed97b740536fd370065f2aa4f72bd8a12e5
Thomas Blarre <thomas@blarre.net>
2026-09-22 12:13:47 +0200

    Install argononed - try #1

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,6 +16,9 @@ cp -avf "/ctx/system_files"/. /
 dnf5 install -y tmux htop cockpit cockpit-podman cockpit-ostree
 # for the argonone daemon
 dnf5 install -y gcc dtc git bash kernel-headers make
+echo VARIANT_ID="iot" >> /etc/os-release
+git clone https://gitlab.com/DarkElvenAngel/argononed.git /root/argononed
+bash /root/argononed/install
 
 # Use a COPR Example:
 #
