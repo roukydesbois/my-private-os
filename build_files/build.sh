329f6db0af6c76c9065dd1850e2a88118eb8f2b1
Thomas Blarre <thomas@blarre.net>
2026-09-23 14:47:59 +0200

    More packages for wifi

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ chmod 600 /usr/lib/NetworkManager/system-connections/OpenAP.conf
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install packages for wifi
-dnf5 install -y wireless-regdb
+dnf5 install -y NetworkManager-wifi wpa_supplicant wireless-regdb
 # Install regular packages
 dnf5 install -y htop cockpit cockpit-podman cockpit-ostree
 # Install dependencies for argon one - maybe python-gpiozero is needed
