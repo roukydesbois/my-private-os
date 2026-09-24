2e0de2273108f8869d527d604c8004915c854293
Thomas Blarre <thomas@blarre.net>
2026-09-24 15:13:01 +0200

    Add rsync

--- build_files/build.sh
+++ build_files/build.sh
@@ -35,7 +35,7 @@ systemctl --global add-wants niri.service dms
 dnf5 -y copr disable avengemedia/dms
 
 # Install cli tools
-dnf5 install -y fish helix git
+dnf5 install -y fish helix git rsync
 
 # Install useful graphical applications
 dnf5 install -y firefox thunderbird
